c65059fc6562cff5afb2c005b28eaed561a5f36a
Eduardo Henrique <duvrdx@gmail.com>
2024-03-01 09:54:47 -0300

    ci/cd: fixing ci

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ with codecs.open(os.path.join(here, "readme.md"), encoding="utf-8") as fh:
     long_description = "\\n" + fh.read()
 
 setup(
-    version='{{VERSION_PLACEHOLDER}}',
     long_description_content_type="text/markdown",
     long_description=long_description,
 )
